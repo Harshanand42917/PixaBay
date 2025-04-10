4897d2956e4e067b25e52d747bef25d3aaa054ae
Harsh Anand <105061582+Harshanand42917@users.noreply.github.com>
2025-04-10 20:04:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Images from "./Components/Images";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
